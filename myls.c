494a0c6ea0783a937b01d291682196b35cc4bd07
Filip Płacheciński <filip0x22@mat.umk.pl>
2019-11-09 00:18:07 +0100

    Nazwa Uzytkownika I Grupy

--- myls.c
+++ myls.c
@@ -3,7 +3,17 @@
 #include <sys/stat.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pwd.h>
+#include <unistd.h>
+#include <grp.h>
 
+
+#define S_IFREG  0100000  /* regular */
+#define S_IFDIR  0040000  /* directory */
+#define S_IFLNK  0120000  /* symbolic link */
+#define S_IFBLK  0060000  /* block special */
+#define S_IFCHR  0020000  /* character special */
+#define S_IFIFO  0010000  /* named pipe (fifo) */
 
 /* Struktura Przechowujaca Dane o Pojednycznym Pliku ! */
 typedef struct{
@@ -11,6 +21,20 @@ typedef struct{
 	struct stat dokumentstat;
 }plik;
 
+/* */
+char* nazwaUzytkownika(unsigned int uid)
+{
+    struct passwd *pws;
+    pws = getpwuid(uid);
+        return pws->pw_name;
+}
+
+char* nazwaGrupy(unsigned int uid)
+{
+    struct group *grupa;
+    grupa = getgrgid(uid);
+        return grupa->gr_name;
+}
 
 /* Funkcja Porownujaca na Potrzeby Uzytego Qsorta */
 int porownanie(const void *a, const void*b){
@@ -21,44 +45,48 @@ int porownanie(const void *a, const void*b){
 
 }
 
-/* Funkcja Konwertująca st_mode pliku na czytelną wersję  */
+/*Funkcja Zwracająca typ pliku */
+static int filetypeletter(unsigned int mode)
+{
+    char c;
 
-/*char *uprawnienia(unsigned int Prawa){
+    if (mode & S_IFREG)
+        c = '-';
+    else if (mode & S_IFDIR)
+        c = 'd';
+    else if (mode & S_IFBLK)
+        c = 'b';
+    else if (mode & S_IFCHR)
+        c = 'c';
+    else if (mode & S_IFIFO)
+        c = 'p';
+    else if (mode & S_IFLNK)
+        c = 'l';
+    else
+    {
+       
+        c = '?';
+    }
+    return(c);
+}
+
+
+/* Funkcja Konwertująca st_mode pliku na czytelną wersję  */
+void uprawnienia(unsigned int Prawa){
 	char buffer[25];
-	char typpliku;
-	if(Prawa & S_IFREG){
-		typpliku = '-';
-	}
-	if(Prawa & S_IFDIR){
-		typpliku = 'd';
-	}
-	if(Prawa & S_IFLNK){
-		typpliku = 'l';
-	}
-	if(Prawa & S_IFIFO){
-		typpliku = 'p';
-	}
-	if(Prawa & S_IFSOCK){
-		typpliku = 's';
-	}
-	if(Prawa & S_IFCHR){
-		typpliku = 'c';
-	}if(Prawa & S_IFBLK){
-		typpliku = 'b';
-	}
-	
-	sprintf(buffer,"%c %c %c %c %c %c %c %c %c %c",typpliku,
+	char typpliku = filetypeletter(Prawa);
+	sprintf(buffer,"%c%c%c%c%c%c%c%c%c%c",typpliku,
 			(Prawa & S_IRUSR ? 'r' : '-'),
 			(Prawa & S_IWUSR ? 'w' : '-'),
 			(Prawa & S_IXUSR ? 'x' : '-'),
-			(Prawa & S_IRGRP ? 'x' : '-'),
-			(Prawa & S_IWGRP ? 'x' : '-'),
+			(Prawa & S_IRGRP ? 'r' : '-'),
+			(Prawa & S_IWGRP ? 'w' : '-'),
 			(Prawa & S_IXGRP ? 'x' : '-'),
-			(Prawa & S_IROTH ? 'x' : '-'),
-			(Prawa & S_IWOTH ? 'x' : '-'),
+			(Prawa & S_IROTH ? 'r' : '-'),
+			(Prawa & S_IWOTH ? 'w' : '-'),
 			(Prawa & S_IXOTH ? 'x' : '-'));
-	return buffer;
-}*/
+	printf("%s", buffer);
+}
 
 
 int main(){
@@ -74,6 +102,7 @@ int main(){
 		return 1;
 	}
 	
+	/* Pętla Zliczająca Ilość Plików W Katalogu */
 	for(;;){
 		direntp = readdir(dirp);
 		if(direntp == NULL) break;
@@ -81,8 +110,11 @@ int main(){
 	}
 	
 	rewinddir(dirp); 
+	/* Dynamiczna Alokcja Pamięci dla Tablocy Zawierającej Informacje o Plikach */
     tablicaPlikow = malloc(sizeof(plik) * iloscplikow);
 	
+	
+	/* Pętla Zapisująca Informacje o Plikach do Struktury */
 	for(;;){
 		direntp = readdir(dirp);
 		if(direntp == NULL) break;
@@ -95,7 +127,12 @@ int main(){
 	qsort(tablicaPlikow,iloscplikow,sizeof(plik),porownanie); /* QSort Uzyty do Posortowania Nazw Plikow W Porzadku Alfabetycznym !! */
 
 	for(i = 0; i < iloscplikow; i++){
-		printf("%u %s\n",tablicaPlikow[i].dokumentstat.st_mode,tablicaPlikow[i].nazwa);
+		uprawnienia(tablicaPlikow[i].dokumentstat.st_mode),
+		printf(" %ld %s %s %s\n",
+		tablicaPlikow[i].dokumentstat.st_nlink,
+		nazwaUzytkownika(tablicaPlikow[i].dokumentstat.st_uid),
+		nazwaGrupy(tablicaPlikow[i].dokumentstat.st_gid),
+		tablicaPlikow[i].nazwa);
 	
 	}
 	
