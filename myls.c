0247f7a0902c7c5b47d4a2a667550af38e29328a
Filip Płacheciński <filip0x22@mat.umk.pl>
2019-11-09 12:49:58 +0100

    AutomatycznaSzerokoscKolumn

--- myls.c
+++ myls.c
@@ -21,7 +21,7 @@ typedef struct{
 	struct stat dokumentstat;
 }plik;
 
-/* */
+/* Funkcja Zwracajaca Nazwę Właściciela jako Parametr Przyjmuje Jego ID */
 char* nazwaUzytkownika(unsigned int uid)
 {
     struct passwd *pws;
@@ -29,6 +29,7 @@ char* nazwaUzytkownika(unsigned int uid)
         return pws->pw_name;
 }
 
+/* Funkcja Zwracajaca Nazwę Grupy jako Parametr Przyjmuje Jej ID */
 char* nazwaGrupy(unsigned int uid)
 {
     struct group *grupa;
@@ -36,7 +37,7 @@ char* nazwaGrupy(unsigned int uid)
         return grupa->gr_name;
 }
 
-/* Funkcja Porownujaca na Potrzeby Uzytego Qsorta */
+/* Funkcja Porownujaca na Potrzeby Uzytego Qsorta sortującego w porządku alfabetycznym*/
 int porownanie(const void *a, const void*b){
 	const plik *pa = (const plik *)a;
 	const plik *pb = (const plik *)b;
@@ -46,7 +47,7 @@ int porownanie(const void *a, const void*b){
 }
 
 /*Funkcja Zwracająca typ pliku */
-static int filetypeletter(unsigned int mode)
+static int znakTypuPliku(unsigned int mode)
 {
     char c;
 
@@ -74,7 +75,7 @@ static int filetypeletter(unsigned int mode)
 /* Funkcja Konwertująca st_mode pliku na czytelną wersję  */
 void uprawnienia(unsigned int Prawa){
 	char buffer[25];
-	char typpliku = filetypeletter(Prawa);
+	char typpliku = znakTypuPliku(Prawa);
 	sprintf(buffer,"%c%c%c%c%c%c%c%c%c%c",typpliku,
 			(Prawa & S_IRUSR ? 'r' : '-'),
 			(Prawa & S_IWUSR ? 'w' : '-'),
@@ -88,57 +89,100 @@ void uprawnienia(unsigned int Prawa){
 	printf("%s", buffer);
 }
 
+void pobierzMaksSzerokoscKolumn(plik tablica[], int rozmiar, int szerokosc[]){
+	int tempszerokosc[2] = {0,0}; 
+	int i = 0;
+	int makspierwsza = (int)tablica[0].dokumentstat.st_nlink;
+	int maksdruga = (int)tablica[0].dokumentstat.st_size;
+	
 
-int main(){
+	for(i = 1; i < rozmiar; i++){
+		if(makspierwsza < (int)tablica[i].dokumentstat.st_nlink){
+			makspierwsza = (int)tablica[i].dokumentstat.st_nlink;
+		
+		}
+	}
+	
+	for(i = 1; i < rozmiar; i++){
+		if(maksdruga < (int)tablica[i].dokumentstat.st_size){
+			maksdruga = (int)tablica[i].dokumentstat.st_size;
+		
+		}
+	}
+	
+	while(makspierwsza != 0){
+		tempszerokosc[0] ++;
+		makspierwsza /= 10;
+	}
+	
+	while(maksdruga != 0){
+		tempszerokosc[1] ++;
+		maksdruga /= 10;
+	}
+	szerokosc[0] = tempszerokosc[0];
+	szerokosc[1] = tempszerokosc[1];
+}
+
+
+int main(int args, char* argv[]){
 	DIR* dirp;
 	struct dirent* direntp;
 	struct stat filestat;
 	plik* tablicaPlikow;
 	int iloscplikow = 0;
-	int i = 0; 
-	
-	if((dirp = opendir(".")) == NULL){
-		perror("Nastapil Blad !!\n");
-		return 1;
-	}
-	
-	/* Pętla Zliczająca Ilość Plików W Katalogu */
-	for(;;){
-		direntp = readdir(dirp);
-		if(direntp == NULL) break;
-		iloscplikow ++;	
-	}
-	
-	rewinddir(dirp); 
-	/* Dynamiczna Alokcja Pamięci dla Tablocy Zawierającej Informacje o Plikach */
-    tablicaPlikow = malloc(sizeof(plik) * iloscplikow);
+	int i = 0;
+	int szerokoscKolumn[2];
 	
 	
-	/* Pętla Zapisująca Informacje o Plikach do Struktury */
-	for(;;){
-		direntp = readdir(dirp);
-		if(direntp == NULL) break;
-		stat(direntp->d_name,&filestat);
-		tablicaPlikow[i].dokumentstat = filestat;
-		sprintf(tablicaPlikow[i].nazwa, "%s", direntp->d_name);
-		i++;
-	}
 	
-	qsort(tablicaPlikow,iloscplikow,sizeof(plik),porownanie); /* QSort Uzyty do Posortowania Nazw Plikow W Porzadku Alfabetycznym !! */
+	if(args == 1){
+		
+		if((dirp = opendir(".")) == NULL){
+			perror("Nastapil Blad !!\n");
+			return 1;
+		}
+		
+		/* Pętla Zliczająca Ilość Plików W Katalogu */
+		for(;;){
+			direntp = readdir(dirp);
+			if(direntp == NULL) break;
+			iloscplikow ++;	
+		}
+		
+		rewinddir(dirp); 
+		/* Dynamiczna Alokcja Pamięci dla Tablocy Zawierającej Informacje o Plikach */
+		tablicaPlikow = malloc(sizeof(plik) * iloscplikow);
+		
+		
+		/* Pętla Zapisująca Informacje o Plikach do Struktury */
+		for(;;){
+			direntp = readdir(dirp);
+			if(direntp == NULL) break;
+			stat(direntp->d_name,&filestat);
+			tablicaPlikow[i].dokumentstat = filestat;
+			sprintf(tablicaPlikow[i].nazwa, "%s", direntp->d_name);
+			i++;
+		}
+		
+		qsort(tablicaPlikow,iloscplikow,sizeof(plik),porownanie); /* QSort Uzyty do Posortowania Nazw Plikow W Porzadku Alfabetycznym !! */
+		pobierzMaksSzerokoscKolumn(tablicaPlikow,iloscplikow,szerokoscKolumn);
 
-	for(i = 0; i < iloscplikow; i++){
-		uprawnienia(tablicaPlikow[i].dokumentstat.st_mode),
-		printf(" %2ld %s  %s %6ld %s\n",
-		(unsigned long)tablicaPlikow[i].dokumentstat.st_nlink,
-		nazwaUzytkownika(tablicaPlikow[i].dokumentstat.st_uid),
-		nazwaGrupy(tablicaPlikow[i].dokumentstat.st_gid),
-		tablicaPlikow[i].dokumentstat.st_size,
-		tablicaPlikow[i].nazwa);
-	
+		for(i = 0; i < iloscplikow; i++){
+			uprawnienia(tablicaPlikow[i].dokumentstat.st_mode),
+			printf(" %*ld %s  %s %*ld %s\n",szerokoscKolumn[0],
+			(unsigned long)tablicaPlikow[i].dokumentstat.st_nlink,
+			nazwaUzytkownika(tablicaPlikow[i].dokumentstat.st_uid),
+			nazwaGrupy(tablicaPlikow[i].dokumentstat.st_gid),
+			szerokoscKolumn[1],
+			tablicaPlikow[i].dokumentstat.st_size,
+			tablicaPlikow[i].nazwa);
+		
+		}
+		
+			free(tablicaPlikow);
+			closedir(dirp);
 	}
 	
 
-	free(tablicaPlikow);
-	closedir(dirp);
 	return 0;
 }
